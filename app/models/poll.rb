353725a30ea8198b598eb3e5ec760058f2714412
Vladimir Khomich <biofiz@gmail.com>
2018-01-26 12:16:53 +0100

    Add Devise gem and User model

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,5 +1,5 @@
 class Poll < ApplicationRecord
-  # belongs_to :user
+  belongs_to :user
 
-  # validates_presence_of :title, :user_id
+  validates_presence_of :title, :user_id
 end
--- db/migrate/20180126103116_create_polls.rb
+++ db/migrate/20180126103116_create_polls.rb
@@ -1,7 +1,7 @@
 class CreatePolls < ActiveRecord::Migration[5.1]
   def change
     create_table :polls do |t|
-      t.string :title, null: false
+      t.string :title,    null: false
       t.integer :user_id, null: false
       t.string :vote_link
 
